806fe6b41d466178b85978cda2446bec127c697d
giuliabee <giuliaboggiali@gmail.com>
2019-01-18 18:33:20 +0100

    Fixed HTML layout and added CSS using Bootstrap framework

--- public/index.js
+++ public/index.js
@@ -2,36 +2,36 @@
 'use strict';
 
 (() => {
-  const render = (actors) => {
-    const fragment = document.createDocumentFragment();
-    const div = document.createElement('div');
-    const template = actors.map(actor => {
-      return `
+    const render = (actors) => {
+        const fragment = document.createDocumentFragment();
+        const div = document.createElement('div');
+        const template = actors.map(actor => {
+            return `
         <div class="actor">
           <span>${actor.who}</span>
           <span>${actor.type}</span>
           <span>${actor.amount}</span>
         </div>
       `;
-    }).join('');
+        }).join('');
 
-    div.innerHTML = template;
-    fragment.appendChild(div);
-    document.querySelector('#actors').innerHTML = '';
-    document.querySelector('#actors').appendChild(fragment);
-  };
+        div.innerHTML = template;
+        fragment.appendChild(div);
+        document.querySelector('#actors').innerHTML = '';
+        document.querySelector('#actors').appendChild(fragment);
+    };
 
-  const button = document.querySelector('#compute');
+    const button = document.querySelector('#compute');
 
-  button.addEventListener('click', function onClick () {
-    const bar = PRIVATEASER.getBar();
-    const time = document.querySelector('.js-time').value;
-    const persons = document.querySelector('.js-persons').value;
-    const option = document.querySelector('.js-option').checked;
-    const actors = PRIVATEASER.payActors(bar, time, persons, option);
+    button.addEventListener('click', function onClick() {
+        const bar = PRIVATEASER.getBar();
+        const time = document.querySelector('.js-time').value;
+        const persons = document.querySelector('.js-persons').value;
+        const option = document.querySelector('.js-option').checked;
+        const actors = PRIVATEASER.payActors(bar, time, persons, option);
 
-    render(actors);
+        render(actors);
 
-    return;
-  });
+        return;
+    });
 })();
